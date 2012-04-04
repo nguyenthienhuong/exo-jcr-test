33096b195f64636124ba6e5274ec7a479066b21d
NGUYEN Thien Huong <nguyenthienhuong0305@gmail.com>
2012-04-04 10:12:53 +0200

    Commit pom.xml

---
+++ src/main/java/com/exoplatform/jcr/App.java
@@ -0,0 +1,13 @@
+package com.exoplatform.jcr;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
